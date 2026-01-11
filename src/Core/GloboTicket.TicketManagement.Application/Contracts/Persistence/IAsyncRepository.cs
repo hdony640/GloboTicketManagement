ad017800ad454e6e74d87b8a87c49c19a15e112d
hdony640 <hadriendony@gmail.com>
2026-01-11 13:23:16 +0100

    feature/GenericRepoInterface

---
+++ src/Core/GloboTicket.TicketManagement.Application/Contracts/Persistence/IAsyncRepository.cs
@@ -0,0 +1,12 @@
+namespace GloboTicket.TicketManagement.Application.Contracts.Persistence
+{
+    //Base repository interface for generic methods
+    public interface IAsyncRepository<T> where T : class
+    {
+        Task<T> GetByIdAsync(Guid id);
+        Task<IReadOnlyList<T>> ListAllAsync();
+        Task<T> AddAsync(T entity);
+        Task UpdateAsync(T entity);
+        Task DeleteAsync(T entity);
+    }
+}
